655c23a7c31c3eb1ef47f59827cfaf5813617b1f
Zephyr <nils.jaudon@ynov.com>
2023-06-17 16:17:58 +0200

    Boucle add joueur

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 // objet joueur, il un nom string, un score int
-// list de joueur.
-
+// liste de joueurs.
 let player = {
-    nom : String,
-    score : 0,
+    nom: String,
+    score: 0,
 }
+const prompt = require('prompt-sync')();
+let nbPlayer = 0;
+let playerList = [];
+
+nbPlayer = parseInt(prompt('What is the number of players: ')); // parseInt permet de convertir une chaine en num
+console.log(`Hey there ${nbPlayer}`);
+
+while (nbPlayer < 1 || nbPlayer > 6) {
+    nbPlayer = parseInt(prompt('Please enter a valid number of players (1-6): '));
+    console.log(`Hey there ${nbPlayer}`);
+}
+
+playerList = [];
+
+for (let index = 0; index < nbPlayer; index++) {
+    const nom = prompt("What is the name of player " + (index + 1) + ": ");
+    let player = { nom: nom };
+    playerList.push(player.nom);
+}
+
+console.log(playerList);
+
