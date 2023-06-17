6ec5e91569a1c832145be31d76920575b93fbd54
Zephyr <nils.jaudon@ynov.com>
2023-06-17 17:19:01 +0200

    AddScorePlayer

--- index.js
+++ index.js
@@ -9,31 +9,32 @@ let nbPlayer = 0;
 let playerList = [];
 
 nbPlayer = parseInt(prompt('What is the number of players: ')); // parseInt permet de convertir une chaine en num
-console.log(`Hey there ${nbPlayer}`);
+console.log(`Hey there  we are ${nbPlayer} to play`);
 
 
 while (nbPlayer < 1 || nbPlayer > 6) {
     nbPlayer = parseInt(prompt('Please enter a valid number of players (1-6): '));
-    console.log(`Hey there ${nbPlayer}`);
+
 }
 
 playerList = [];
 
 for (let index = 0; index < nbPlayer; index++) {
     const nom = prompt("What is the name of player " + (index + 1) + ": ");
-    let player = { nom: nom };
-    playerList.push(player.nom);
+    let newplayer = { nom: nom,
+                   score: 0,};
+    playerList.push(newplayer);
 }
 
-for (let frame = 0; frame < 10; frame++) {
-    console.log("Vous etes au tour : " + (frame + 1))
+for (let frame = 0; frame < 1; frame++) {
+    console.log("Vous etes au tour : " + (frame + 1) + "\n \n \n")
     for (let iteratePlayer = 0; iteratePlayer < playerList.length; iteratePlayer++) {
-
-        quilleFall = parseInt(prompt('how many quilles fall ' +playerList[iteratePlayer] + ' : '));
-
+        for (let throwFrame = 0; throwFrame < 2 ;throwFrame++) {
+            console.log("Frame" + (frame + 1) + "Lancer" + (throwFrame+1))
+            keelFall = parseInt(prompt('how many keels fall ' + playerList[iteratePlayer].nom + ' : '));
+            playerList[iteratePlayer].score += keelFall
+        }   
     }
-  
-    
 }
-console.log(playerList);
 
+console.log(playerList)
