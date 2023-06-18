b05145b17145f658c8358c1618a762cac2a4d722
Zephyr <nils.jaudon@ynov.com>
2023-06-18 17:55:50 +0200

    Spare

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let ResultStrike = 0;
 let nbPlayer = 0;
 let playerList = [];
 let throwplayer
+
+
 // Question est posée dans le CLI, on récupère l'information utilisateur sous la forme d'un Integer.
 nbPlayer = parseInt(prompt('How many players are we? '));
 
@@ -22,44 +24,71 @@ for (let index = 0; index < nbPlayer; index++) {
         nom: nom,
         score: [],
         throw: 0,
+        keels: 10,
+        ifspare: Boolean,
+        ifStrike: Boolean
     };
     playerList.push(newPlayer);
-    
+
 }
-for (let frame = 0; frame < 10; frame++) {
+for (let frame = 0; frame < 2; frame++) {
     console.log("Vous êtes au tour " + (frame + 1) + "\n\n\n");
     for (let iteratePlayer = 0; iteratePlayer < playerList.length; iteratePlayer++) {
+
         for (let throwFrame = 0; throwFrame < 2; throwFrame++) {
             console.log("\n Frame " + (frame + 1) + ", Lancer " + (throwFrame + 1) + "\n");
-            
+
             const pinsFall = parseInt(prompt('How many pins fell for ' + playerList[iteratePlayer].nom + ": "));
-            
+
             if (pinsFall == 10 && throwFrame === 0) {
                 console.log("\n Strike ! +10 au 2 prochain lancer ! \n");
             }
-            
+
             const throwplayer = playerList[iteratePlayer].throw;
-           
-            
-            
+
+
+
             playerList[iteratePlayer].throw += 1;
-            if (throwFrame === 0 && playerList[iteratePlayer].score[throwplayer-2] === 10 
-                || throwFrame === 0 && (playerList[iteratePlayer].score[throwplayer -2] - 10) >= 10
-                || throwFrame === 1 && playerList[iteratePlayer].score[throwplayer -1] === 10 || throwFrame === 1 && (playerList[iteratePlayer].score[throwplayer -1] - 10) >= 10) {
+
+            if (throwFrame === 0 && playerList[iteratePlayer].score[throwplayer - 2] === 10 ||
+                throwFrame === 0 && (playerList[iteratePlayer].score[throwplayer - 2] - 10) >= 10 ||
+                throwFrame === 1 && playerList[iteratePlayer].score[throwplayer - 1] === 10 ||
+                throwFrame === 1 && (playerList[iteratePlayer].score[throwplayer - 1] - 10) >= 10) {
                 ResultStrike = pinsFall + 10;
                 playerList[iteratePlayer].score.push((ResultStrike));
                 ResultStrike = 0;
-             }
+                playerList[iteratePlayer].keels -= pinsFall
+                console.log("\n Quille restante " + playerList[iteratePlayer].keels)
+                playerList[iteratePlayer].keels = 10
+                playerList[iteratePlayer].ifStrike = true
+            } else {
 
-             
-             
-              else {
-                 playerList[iteratePlayer].score.push(pinsFall);
-             }
-             console.log(playerList)
+                playerList[iteratePlayer].keels -= pinsFall
+                console.log("\n Quille restante " + playerList[iteratePlayer].keels)
+                if (playerList[iteratePlayer].keels === 0 && throwFrame === 1) {
+                    console.log("S P A R E ! Prochain coup + 10 ")
+                    playerList[iteratePlayer].ifspare = true
+
+                }
+                if (playerList[iteratePlayer].ifspare == true && throwFrame === 0) {
+                    playerList[iteratePlayer].score.push(pinsFall + 10)
+                    playerList[iteratePlayer].ifspare == false
+                } else {
+                    playerList[iteratePlayer].score.push(pinsFall)
+                }
+
+            }
+
+
+            if (throwFrame === 2) {
+                playerList[iteratePlayer].keels = 10
+            }
+
         }
 
     }
+
+
 }
 
 console.log(playerList);
